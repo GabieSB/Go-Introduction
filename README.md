87c108d2147e652dd31eeffe412eb599e08958ee
GabieSB <serracin.monica@gmail.com>
2021-08-23 13:27:36 -0600

    declaring and intialization of variables

---
+++ README.md
@@ -0,0 +1,5 @@
+# Get started.
+1. Download and install withs https://golang.org/doc/install?download=go1.12.7.windows-amd64.msi 
+2. run the command ``` code main.go```
+
+
---
+++ section2/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// for the prints
+
+func main() {
+	var m1 int // declare a variable
+	m1 = 2
+	fmt.Println((m1))
+
+	//declare multiples variables
+	var (
+		m3 = 2
+		m4 = 3
+	)
+
+	m6 := 5
+
+	fmt.Println(m3 + m4 + m6)
+}
